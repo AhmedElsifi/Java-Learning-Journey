764d7d4d8884bea6a93003e2849337febc463c61
AhmedElsifi <ahmedelsifi06@gmail.com>
2025-09-18 17:42:00 -0700

    adding chapter 10 task files first try

--- Practicing/Student Grade Analyzer/src/App.java
+++ Practicing/Student Grade Analyzer/src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many Students will you process ? ");
+        int noStudents = input.nextInt();
+        Student[] students = new Student[noStudents];
+
     }
 }
--- Practicing/Student Grade Analyzer/src/Student.java
+++ Practicing/Student Grade Analyzer/src/Student.java
@@ -1,3 +1,20 @@
-public class Student {
+import java.math.BigDecimal;
 
+public class Student {
+    StringBuilder name;
+    BigDecimal grade;
+
+    public Student() {
+        this.name = new StringBuilder("no name is set");
+        this.grade = new BigDecimal(0);
+    }
+
+    public Student(String name, int grade) {
+        this.name = new StringBuilder(name);
+        this.grade = new BigDecimal(grade);
+    }
+
+    public void printStudentDetails() {
+        System.out.printf("%-10s %-10d \n", this.name, this.grade);
+    }
 }
